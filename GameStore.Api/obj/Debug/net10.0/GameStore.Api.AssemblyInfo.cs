fddaa7ad44f0dc5132ccb71410c409b7f370065b
isaacaarondavies <isaac.work.davies@gmail.com>
2026-04-14 22:02:21 -0600

    Finished configuration portion

--- GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b67b320e03e46d61469dd0533818ec56e7fd9eff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+068b4e33075ea85d4a24f6e7a06ba9b4820d0fe0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
